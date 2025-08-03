44e3e7cc73d81a4160c2c3f36935d034ee660128
uvramana <uvramana@gmail.com>
2025-08-03 13:43:56 -0700

    Update factors.cpp

--- factors.cpp
+++ factors.cpp
@@ -4,12 +4,18 @@
 int main() {
     // Write C++ code here
     int n;
+    while(1) {
     std::cin >> n;
     std::cout << "Finding factors for: " << n << std::endl;
+    int total = 0;
     for (int i=1; i<=n; i++) {
         if (n%i == 0) {
+            total++;
             std::cout << i << " : " << (n/i) << std::endl;
         }
+    }
+    
+    std::cout << "total factors are " << total << std::endl;
     }
 
     return 0;
