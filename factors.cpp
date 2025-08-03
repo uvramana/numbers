fd2e73940b9e8b09d1ff13d7a0618ce04830268d
uvramana <uvramana@gmail.com>
2025-08-03 13:34:15 -0700

    Update factors.cpp
    Factors

--- factors.cpp
+++ factors.cpp
@@ -1,1 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
 
+int main() {
+    // Write C++ code here
+    int n;
+    std::cin >> n;
+    std::cout << "Finding factors for: " << n << std::endl;
+    for (int i=1; i<=n; i++) {
+        if (n%i == 0) {
+            std::cout << i << " : " << (n/i) << std::endl;
+        }
+    }
+
+    return 0;
+}
